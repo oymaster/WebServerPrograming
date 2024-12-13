d6ccfafade86d4944d94843a0e5773ad1e36eca9
oymaster <1612251970@qq.com>
2024-12-13 16:44:18 +0800

    2024-11-04 15:28:34 by desktop

---
+++ ch14/14_5_pthread_signal.cpp
@@ -0,0 +1,39 @@
+#include<pthread.h>
+#include<stdio.h>
+#include<unistd.h>
+#include<signal.h>
+#include<errno.h>
+#include<stdlib.h>
+
+// "\"的作用是延续下一行
+#define handle_error_en(en,msg) \ 
+        do{ errno = en; perror(msg);exit(EXIT_FAILURE);} while(0);
+
+static void *sig_thread(void *arg){
+    sigset_t *set = (sigset_t *) arg;
+    int s,sig;
+    for(;;){
+        s = sigwait(set,&sig);
+        if(s != 0) handle_error_en(s,"sigwait");
+        printf("Signal handling thread got signal %d\n",sig);
+    }
+}
+
+int main(int argc,char *argv[]){
+    pthread_t thread;
+    sigset_t set;
+    int s;
+    sigemptyset(&set);
+    sigaddset(&set,SIGQUIT);  ,,
+    sigaddset(&set,SIGUSR1);
+    
+    s = pthread_sigmask(SIG_BLOCK,&set,NULL);
+    if( s!=0 ) 
+        handle_error_en(s,"pthread_sigmask");
+    
+    s = pthread_create(&thread,NULL,&sig_thread,(void *)&set);
+    if( s!=0 ) 
+        handle_error_en(s,"pthread_create");
+    
+    pause();
+}
